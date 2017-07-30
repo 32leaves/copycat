b71daba80d65e546f36c160301b1166411c0a401
32leaves <github@32leav.es>
2017-07-30 18:47:53 +0200

    Minor UI tweaks

--- app.js
+++ app.js
@@ -14,80 +14,68 @@
 
 'use strict';
 
+var os = require('os');
 var DriveManager = require('./lib/drivemanager');
 var FakeDriveManager = require('./lib/fakedrivemanager');
 var CopyManager = require('./lib/copymanager');
 var WebUI = require('./lib/webui');
 
 
-
-const driveManager = new DriveManager((drive) => {
-    if(drive.description.indexOf('Backup') > -1) {
-        return 0;
-    } else if(drive.name.indexOf('OutAndAbout') > -1) {
-        return 10;
-    } else {
-        return 20;
-    }
-});
-//const driveManager = new FakeDriveManager();
-driveManager.on('driveUpdate', (added, removed) => {
-    console.log('Drives added ', added);
-    console.log('Drives removed ', removed);
-});
-
-const copyManager = new CopyManager(driveManager);
-
+// start webui first to ease debugging
 const ui = new WebUI();
 ui.start();
 
-driveManager.on('driveUpdate', (added, removed) => {
-    ui.setDrives(driveManager.drives);
-});
-copyManager.on('newJob', (job) => {
-    console.log('New job', job);
-    ui.createJob(job.id(), job.source);
+if(os.type() == 'Windows_NT') {
+    console.log('Running on windows. Starting WebUI only.');
+    setInterval(() => {
+        var id = Math.floor(Math.random() * 1000);
+        ui.createJob(id, '/media/' + id);
+        ui.setProgress(id, Math.floor(Math.random() * 100));
 
-    job.on('error', (err) => {
-        console.log('Error', job.id(), err);
-        ui.setError(job.id(), err.toString());
+        const rand = Math.random();
+        if(rand < 0.3) {
+            ui.setSyncing(id);
+        }
+    }, 5000);
+} else {
+    const driveManager = new DriveManager((drive) => {
+        if(drive.description.indexOf('Backup') > -1) {
+            return 0;
+        } else if(drive.name.indexOf('OutAndAbout') > -1) {
+            return 10;
+        } else {
+            return 20;
+        }
     });
-    job.on('progress', (perc) => {
-        console.log('Progress', job.id(), perc.percentage);
-        ui.setProgress(job.id(), perc.percentage);
+    driveManager.on('driveUpdate', (added, removed) => {
+        console.log('Drives added ', added);
+        console.log('Drives removed ', removed);
     });
-    job.on('sync', () => {
-        console.log('Sync', job.id());
-        ui.setSyncing(job.id());
+
+    const copyManager = new CopyManager(driveManager);
+
+    driveManager.on('driveUpdate', (added, removed) => {
+        ui.setDrives(driveManager.drives);
     });
-    job.on('done', () => {
-        console.log('Done', job.id());
-        ui.setDone(job.id());
+    copyManager.on('newJob', (job) => {
+        console.log('New job', job);
+        ui.createJob(job.id(), job.source);
+
+        job.on('error', (err) => {
+            console.log('Error', job.id(), err);
+            ui.setError(job.id(), err.toString());
+        });
+        job.on('progress', (perc) => {
+            console.log('Progress', job.id(), perc.percentage);
+            ui.setProgress(job.id(), perc.percentage);
+        });
+        job.on('sync', () => {
+            console.log('Sync', job.id());
+            ui.setSyncing(job.id());
+        });
+        job.on('done', () => {
+            console.log('Done', job.id());
+            ui.setDone(job.id());
+        });
     });
-});
-
-/*
-setTimeout(() => {
-driveManager.addDrive({'priority': 10, 'mountpoint':'/tmp/dest', 'name':'dest'});
-driveManager.addDrive({'priority': 20, 'mountpoint':'/tmp/src', 'name':'src'});
-}, 5000);
-*/
-
-// var jobid = 'foobar';
-// setTimeout(() => {
-//     ui._createJob(jobid, jobid);
-// }, 5000);
-// setTimeout(() => {
-//     ui._setProgress(jobid, 22);
-// }, 10000);
-// setTimeout(() => {
-//     ui._setDone(jobid);
-// }, 15000);
-
-// const manager = new DriveManager('Out And About');
-// manager.on('driveUpdate', (added, removed) => {
-//     console.log('Added ', added);
-//     console.log('Removed ', removed);
-
-    
-// });
+}
